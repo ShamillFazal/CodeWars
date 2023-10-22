57f1f4a7a9d467a427dffce55f3753fa7043bf66
unknown <shamy_pw4@hotmail.co.uk>
2023-10-22 10:36:22 +0100

    adding old completed katas to repo

---
+++ 8kyu/Even_or_Odd.js
@@ -0,0 +1,11 @@
+// Function to check if a number is even or odd.
+
+function evenOrOdd(number) {
+    // Check if the number is divisible by 2 (even).
+    if (number % 2 === 0) {
+      return "Even";  // Return "Even" if it's even.
+    } else {
+      return "Odd";   // Return "Odd" if it's odd.
+    }
+  }
+  
---
+++ 8kyu/Multiply.js
@@ -0,0 +1,6 @@
+// Simple JavaScript function named 'multiply' that multiplies two numbers.
+
+function multiply(a, b) {
+  // Multiply 'a' and 'b' and return the result.
+  return a * b;
+}
