6cadf955e25be82561616b713275f74225eaa8a6
unknown <shamy_pw4@hotmail.co.uk>
2023-10-22 10:40:35 +0100

    adding more old completed katas

---
+++ 8kyu/Convert_A_Number_To_A_String.js
@@ -0,0 +1,12 @@
+// Convert a number to a string.
+
+function numberToString(num) {
+
+  // Use the 'toString' method to change 'num' to a string.
+
+  let stringify = num.toString();
+
+  // Return the converted string.
+  
+  return stringify;
+}
---
+++ 8kyu/Opposite_Number.js
@@ -0,0 +1,12 @@
+function opposite(number) {
+    
+    /*- Take a number 
+      - Multiply chosen number by 2
+      - Create variable (doubled) and assign output of product above
+      - Take initial number and subtract doubled variable
+      - return */
+
+    let doubled = number * 2;
+      return number - doubled
+    }
+    
